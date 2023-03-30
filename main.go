abc2c41d5e6aadb28938e4d7ede134e5688c3ac2
rajatjain007 <rajat.apple007@gmail.com>
2023-03-30 00:49:22 -0700

    ChatGPT integration

--- main.go
+++ main.go
@@ -2,59 +2,63 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
+	gpt3 "github.com/PullRequestInc/go-gpt3"
 	"github.com/joho/godotenv"
-	"github.com/krognol/go-wolfram"
 	"github.com/shomali11/slacker"
-	"github.com/tidwall/gjson"
-	witai "github.com/wit-ai/wit-go/v2"
 )
 
-var wolframClient *wolfram.Client
-
-func main(){
+func main() {
 	godotenv.Load(".env")
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"),os.Getenv("SLACK_APP_TOKEN"))
-	witAIClient := witai.NewClient(os.Getenv("WITAI_TOKEN"))
-	wolframClient = &wolfram.Client{AppID:os.Getenv("WOLFRAM_APP_ID")}
-	
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("Empty API_KEY")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("<query>",&slacker.CommandDefinition{
-		Description: "Send any question to Wolfram",
-		Examples: []string{"who is cristiano ronaldo?"},
+	bot.Command("<query>", &slacker.CommandDefinition{
+		Description: "Send any question to ChatGPT",
+		Examples:    []string{"who is cristiano ronaldo?"},
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("query")
-			message,_ := witAIClient.Parse(&witai.MessageRequest{
-				Query: query,
-			})
-			data, _ := json.MarshalIndent(message,"","    ")
-			rough := string(data[:])
-			value := gjson.Get(rough,"entities.wit$wolfram_search_query:wolfram_search_query.1.value")
-			answer := value.String()
-			res,err := wolframClient.GetSpokentAnswerQuery(answer,wolfram.Metric,1000)
-			if(err != nil){
-				fmt.Println("Error from Wolfram")
-			}
+			res := GetResponse(client, ctx, query)
 			response.Reply(res)
 		},
 	})
 
-	ctx,cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err:=bot.Listen(ctx)
-	if(err!=nil){
+	err := bot.Listen(ctx)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func printCommandEvents(analyticsChannel <- chan *slacker.CommandEvent){
-	for event := range analyticsChannel{
+func GetResponse(client gpt3.Client, ctx context.Context, question string) string {
+	resp, err := client.CompletionWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt:      []string{question},
+		MaxTokens:   gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	})
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	return resp.Choices[0].Text
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
 		fmt.Println("Command Eventds")
 		fmt.Println(event.Timestamp)
 		fmt.Println(event.Command)
